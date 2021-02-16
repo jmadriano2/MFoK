1835eac7feb85252fc68d6e3be46d1e6d98fd5f8
micmanan <micmanan@MAN-LF6FV4S2.misys.global.ad>
2021-02-16 16:22:49 +0800

    Collpsible list

--- resources/js/components/coblogs/PreCob.vue
+++ resources/js/components/coblogs/PreCob.vue
@@ -2,26 +2,72 @@
   <!-- Pre-CoB Rituals -->
   <div class="row">
     <div class="col-sm-6 offset-sm-3">
-      <ul class="list-group">
-        <li class="list-group-item">
+      <div class="list-group">
+        <a href="#step1" class="list-group-item list-group-item-action" data-toggle="collapse">
           1.0 - Check for Outstanding Transactions
-        </li>
-        <li class="list-group-item">
+        </a>
+        <div class="collapse" id="step1">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#step2" class="list-group-item list-group-item-action" data-toggle="collapse">
           2.0 - Check for objects in xxINVLIB that could affect CoB output
-        </li>
-        <li class="list-group-item">
+        </a>
+        <div class="collapse" id="step2">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#step3" class="list-group-item list-group-item-action" data-toggle="collapse">
           3.0 - Check for requested Halts for bug investigation
-        </li>
-        <li class="list-group-item">4.0 - Run VALD</li>
-        <li class="list-group-item">
+        </a>
+        <div class="collapse" id="step3">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#step4" class="list-group-item list-group-item-action" data-toggle="collapse">
+            4.0 - Run VALD
+        </a>
+        <div class="collapse" id="step4">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#step5" class="list-group-item list-group-item-action" data-toggle="collapse">
           5.0 - Check Queue Managers and Channels are Active
-        </li>
-        <li class="list-group-item">6.0 - Check System Status</li>
-        <li class="list-group-item">
+        </a>
+        <div class="collapse" id="step5">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#ste6" class="list-group-item list-group-item-action" data-toggle="collapse">
+            6.0 - Check System Status
+        </a>
+        <div class="collapse" id="step6">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#step7" class="list-group-item list-group-item-action" data-toggle="collapse">
           7.0 - Sign-in to BFMI to check if WAS is up
-        </li>
-        <li class="list-group-item">8.0 - Clear Message Queues</li>
-      </ul>
+        </a>
+        <div class="collapse" id="step7">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+        <a href="#step8" class="list-group-item list-group-item-action" data-toggle="collapse">
+            8.0 - Clear Message Queues
+        </a>
+        <div class="collapse" id="step8">
+            <div class="card card-body">
+                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
