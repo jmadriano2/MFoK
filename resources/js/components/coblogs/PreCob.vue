a1ca5fa37e61a1b6f17d823d5ae13ac0b040670f
micmanan <micmanan@MAN-LF6FV4S2.misys.global.ad>
2021-02-17 11:49:29 +0800

    20210217_1149
    *Organized the whitespaces on PreCob.vue.

--- resources/js/components/coblogs/PreCob.vue
+++ resources/js/components/coblogs/PreCob.vue
@@ -1,83 +1,83 @@
 <template>
-  <!-- Pre-CoB Rituals List -->
-  <div class="row">
-    <div class="col-sm-10 offset-sm-1">
-      <div class="list-group">
-        <a
-            href="#step1"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem1Expanded}"
-            v-on:click="toggleListItem(1)">
-                1.0 - Check for Outstanding Transactions
-        </a>
-        <PreCobItem1></PreCobItem1>
-        <a
-            href="#step2"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem2Expanded}"
-            v-on:click="toggleListItem(2)">
-                2.0 - Check for objects in xxINVLIB that could affect CoB output
-        </a>
-        <PreCobItem2></PreCobItem2>
-        <a
-            href="#step3"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem3Expanded}"
-            v-on:click="toggleListItem(3)">
-                3.0 - Check for requested halts for bug investigation
-        </a>
-        <PreCobItem3></PreCobItem3>
-        <a
-            href="#step4"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem4Expanded}"
-            v-on:click="toggleListItem(4)">
-                4.0 - Run VALD
-        </a>
-        <PreCobItem4></PreCobItem4>
-        <a
-            href="#step5"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem5Expanded}"
-            v-on:click="toggleListItem(5)">
-                5.0 - Check Queue Managers and Channels are Active
-        </a>
-        <PreCobItem5></PreCobItem5>
-        <a
-            href="#step6"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem6Expanded}"
-            v-on:click="toggleListItem(6)">
-                6.0 - Check System Status
-        </a>
-        <PreCobItem6></PreCobItem6>
-        <a
-            href="#step7"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem7Expanded}"
-            v-on:click="toggleListItem(7)">
-                7.0 - Sign-in to BFMI to check if WAS is up
-        </a>
-        <PreCobItem7></PreCobItem7>
-        <a
-            href="#step8"
-            class="list-group-item list-group-item-action"
-            data-toggle="collapse"
-            :class="{active: isItem8Expanded}"
-            v-on:click="toggleListItem(8)">
-                8.0 - Clear Message Queues
-        </a>
-        <PreCobItem8></PreCobItem8>
-      </div>
+    <!-- Pre-CoB Rituals List -->
+    <div class="row">
+        <div class="col-sm-10 offset-sm-1">
+            <div class="list-group">
+                <a
+                    href="#step1"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem1Expanded}"
+                    v-on:click="toggleListItem(1)">
+                        1.0 - Check for Outstanding Transactions
+                </a>
+                <PreCobItem1></PreCobItem1>
+                <a
+                    href="#step2"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem2Expanded}"
+                    v-on:click="toggleListItem(2)">
+                        2.0 - Check for objects in xxINVLIB that could affect CoB output
+                </a>
+                <PreCobItem2></PreCobItem2>
+                <a
+                    href="#step3"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem3Expanded}"
+                    v-on:click="toggleListItem(3)">
+                        3.0 - Check for requested halts for bug investigation
+                </a>
+                <PreCobItem3></PreCobItem3>
+                <a
+                    href="#step4"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem4Expanded}"
+                    v-on:click="toggleListItem(4)">
+                        4.0 - Run VALD
+                </a>
+                <PreCobItem4></PreCobItem4>
+                <a
+                    href="#step5"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem5Expanded}"
+                    v-on:click="toggleListItem(5)">
+                        5.0 - Check Queue Managers and Channels are Active
+                </a>
+                <PreCobItem5></PreCobItem5>
+                <a
+                    href="#step6"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem6Expanded}"
+                    v-on:click="toggleListItem(6)">
+                        6.0 - Check System Status
+                </a>
+                <PreCobItem6></PreCobItem6>
+                <a
+                    href="#step7"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem7Expanded}"
+                    v-on:click="toggleListItem(7)">
+                        7.0 - Sign-in to BFMI to check if WAS is up
+                </a>
+                <PreCobItem7></PreCobItem7>
+                <a
+                    href="#step8"
+                    class="list-group-item list-group-item-action"
+                    data-toggle="collapse"
+                    :class="{active: isItem8Expanded}"
+                    v-on:click="toggleListItem(8)">
+                    8.0 - Clear Message Queues
+                </a>
+                <PreCobItem8></PreCobItem8>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
