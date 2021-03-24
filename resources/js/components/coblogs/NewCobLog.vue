a0f170ac76e1a0fe2c61be0670ac86322f55d3f5
micmanan <micmanan@MAN-LF6FV4S2.misys.global.ad>
2021-03-24 20:18:22 +0800

    Added validation for second tab.

--- resources/js/components/coblogs/NewCobLog.vue
+++ resources/js/components/coblogs/NewCobLog.vue
@@ -1,158 +1,175 @@
 <template>
-  <div class="col-sm">
-    <form-wizard
-      @on-complete="addNewCobLog"
-      color="#6c7ae0"
-      title="Close of Business Logger"
-      subtitle="New CoB Log"
-      finishButtonText="Start"
-    >
-      <tab-content title="Pre-CoB" icon="fa fa-cog">
-        <PreCob></PreCob>
-      </tab-content>
-      <tab-content
-        title="CoB Info"
-        icon="fa fa-info"
-        :beforeChange="validateSystem">
-        <div class="row">
-          <div class="offset-sm-2 col-sm-8">
-            <div class="card see-through">
-              <div class="row">
-                <div class="offset-sm-1 col-sm-10">
-                  <div class="form-group mb-2 mt-1">
-                    <label for="recipient-name" class="col-form-label mr-4"
-                      >System:</label
-                    >
-                    <div
-                        class="alert alert-danger d-none"
-                        role="alert"
-                        :class="{'d-block': hasError}">
-                      System is required.
-                    </div>
-                    <select
-                      id="systems"
-                      class="form-control"
-                      v-model="selectedSystem"
-                      @change="onSystemChange()"
-                    >
-                      <option
-                        v-for="system in systems"
-                        v-bind:key="system.id"
-                        v-bind:value="{
-                          id: system.id,
-                          machine: system.machine,
-                          system: system.system,
-                          zone: system.zone,
-                        }"
-                      >
-                        {{ system.machine }} - {{ system.system }}/{{
-                          system.zone
-                        }}
-                      </option>
-                    </select>
-                  </div>
+    <div class="col-sm">
+        <form-wizard
+            @on-complete="addNewCobLog"
+            color="#6c7ae0"
+            title="Close of Business Logger"
+            subtitle="New CoB Log"
+            finishButtonText="Start">
 
-                  <div class="form-group mb-2">
-                    <label for="recipient-name" class="col-form-label mr-4" disa
-                      >Current Run Date:</label
-                    >
-                    <input
-                      readonly
-                      type="text"
-                      class="form-control"
-                      id="component-name"
-                      v-model="rundate"
-                      v-on:click="toggleRDCalendar()"
-                    />
-                    <div class="row">
-                      <div class="col-sm-4">
-                        <v-date-picker
-                          v-if="showRDCalendar"
-                          mode="date"
-                          v-model="rundate"
-                          :model-config="modelConfig"
-                        />
-                      </div>
-                      <div
-                        class="col-sm-8"
-                        v-on:click="toggleRDCalendar()"
-                      ></div>
-                    </div>
-                  </div>
+            <!-- First tab -->
+            <tab-content title="Pre-CoB" icon="fa fa-cog">
+                <PreCob></PreCob>
+            </tab-content>
 
-                  <div class="form-group mb-3">
-                    <label for="recipient-name" class="col-form-label mr-4"
-                      >Next Run Date:</label
-                    >
-                    <input
-                      readonly
-                      type="text"
-                      class="form-control"
-                      id="component-name"
-                      v-model="nextRundate"
-                      v-on:click="toggleNRDCalendar()"
-                    />
-                    <div class="row">
-                      <div class="col-sm-4">
-                        <v-date-picker
-                          v-if="showNRDCalendar"
-                          mode="date"
-                          v-model="nextRundate"
-                          :model-config="modelConfig"
-                        />
-                      </div>
-                      <div
-                        class="col-sm-8"
-                        v-on:click="toggleNRDCalendar()"
-                      ></div>
+            <!-- Second tab -->
+            <tab-content
+                title="CoB Info"
+                icon="fa fa-info"
+                :beforeChange="validateSystem">
+
+                <div class="row">
+                    <div class="offset-sm-2 col-sm-8">
+                        <div class="card see-through">
+                            <div class="row">
+                                <div class="offset-sm-1 col-sm-10">
+                                    <!-- System field -->
+                                    <div class="form-group mb-2 mt-1">
+                                        <label
+                                            for="recipient-name"
+                                            class="col-form-label mr-4">
+                                            System:
+                                        </label>
+                                        <!-- System error -->
+                                        <div
+                                            class="alert alert-danger d-none"
+                                            role="alert"
+                                            :class="{'d-block': hasSystemError}">
+                                                System is required.
+                                        </div>
+                                        <!-- System dropdown -->
+                                        <select
+                                            id="systems"
+                                            class="form-control"
+                                            v-model="selectedSystem"
+                                            @change="onSystemChange()">
+                                            <option
+                                                v-for="system in systems"
+                                                v-bind:key="system.id"
+                                                v-bind:value="{
+                                                    id: system.id,
+                                                    machine: system.machine,
+                                                    system: system.system,
+                                                    zone: system.zone,}">
+                                                        {{ system.machine }} - {{ system.system }}/{{system.zone}}
+                                            </option>
+                                        </select>
+                                    </div>
+
+                                    <!-- Rundate field -->
+                                    <div class="form-group mb-2">
+                                        <label for="recipient-name" class="col-form-label mr-4">
+                                            Current Run Date:
+                                        </label>
+                                        <!-- Rundate error -->
+                                        <div
+                                            class="alert alert-danger d-none"
+                                            role="alert"
+                                            :class="{'d-block': hasRDError}">
+                                                Rundate is required.
+                                        </div>
+                                        <input
+                                            readonly
+                                            type="text"
+                                            class="form-control"
+                                            id="component-name"
+                                            v-model="rundate"
+                                            v-on:click="toggleRDCalendar()"/>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <v-date-picker
+                                                    v-if="showRDCalendar"
+                                                    mode="date"
+                                                    v-model="rundate"
+                                                    :model-config="modelConfig"/>
+                                            </div>
+                                            <div
+                                                class="col-sm-8"
+                                                v-on:click="toggleRDCalendar()">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <label for="recipient-name" class="col-form-label mr-4">
+                                            Next Run Date:
+                                        </label>
+                                        <!-- Next Rundate error -->
+                                        <div
+                                            class="alert alert-danger d-none"
+                                            role="alert"
+                                            :class="{'d-block': hasNRDError}">
+                                                Next Rundate is required.
+                                        </div>
+                                        <input
+                                            readonly
+                                            type="text"
+                                            class="form-control"
+                                            id="component-name"
+                                            v-model="nextRundate"
+                                            v-on:click="toggleNRDCalendar()"/>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <v-date-picker
+                                                    v-if="showNRDCalendar"
+                                                    mode="date"
+                                                    v-model="nextRundate"
+                                                    :model-config="modelConfig"/>
+                                            </div>
+                                            <div
+                                                class="col-sm-8"
+                                                v-on:click="toggleNRDCalendar()">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </tab-content>
-      <tab-content title="Start Log" icon="fa fa-list">
-        <div class="row">
-          <div class="col-sm-6 offset-sm-3">
-              <li class="list-group-item">
+            </tab-content>
+
+            <!-- Third tab -->
+            <tab-content title="Start Log" icon="fa fa-list">
                 <div class="row">
-                  <div class="col-sm-4">Machine:</div>
-                  <div class="col-sm-6">{{ selectedSystem.machine }}</div>
+                    <div class="col-sm-6 offset-sm-3">
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-sm-4">Machine:</div>
+                                <div class="col-sm-6">{{ selectedSystem.machine }}</div>
+                            </div>
+                        </li>
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col-sm-4">System:</div>
+                                    <div class="col-sm-6">{{ selectedSystem.system }}</div>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col-sm-4">Zone:</div>
+                                    <div class="col-sm-6">{{ selectedSystem.zone }}</div>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col-sm-4">Current Run Date:</div>
+                                    <div class="col-sm-6">{{ rundate }}</div>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col-sm-4">Next Run Date:</div>
+                                    <div class="col-sm-6">{{ nextRundate }}</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-              </li>
-            <ul class="list-group">
-              <li class="list-group-item">
-                <div class="row">
-                  <div class="col-sm-4">System:</div>
-                  <div class="col-sm-6">{{ selectedSystem.system }}</div>
-                </div>
-              </li>
-              <li class="list-group-item">
-                <div class="row">
-                  <div class="col-sm-4">Zone:</div>
-                  <div class="col-sm-6">{{ selectedSystem.zone }}</div>
-                </div>
-              </li>
-              <li class="list-group-item">
-                <div class="row">
-                  <div class="col-sm-4">Current Run Date:</div>
-                  <div class="col-sm-6">{{ rundate }}</div>
-                </div>
-              </li>
-              <li class="list-group-item">
-                <div class="row">
-                  <div class="col-sm-4">Next Run Date:</div>
-                  <div class="col-sm-6">{{ nextRundate }}</div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </tab-content>
-    </form-wizard>
-  </div>
+            </tab-content>
+        </form-wizard>
+    </div>
 </template>
 
 <script>
@@ -171,7 +188,9 @@ export default {
   },
   data() {
     return {
-        hasError: false,
+        hasSystemError: false,
+        hasRDError: false,
+        hasNRDError: false,
         isClearedToProceed: false,
       systems: [],
       selectedSystem: "",
@@ -240,15 +259,36 @@ export default {
     },
     validateSystem() {
         if (this.selectedSystem == '') {
-            this.hasError = true;
+            this.hasSystemError = true;
+        } else {
+            this.hasSystemError = false;
+        }
+        if (this.rundate == '') {
+            this.hasRDError = true;
+        } else {
+            this.hasRDError = false;
+        }
+        if (this.nextRundate == '') {
+            this.hasNRDError = true;
+        } else {
+            this.hasNRDError = false;
+        }
+
+        console.log('System:' + this.selectedSystem);
+        console.log('Rundate:' + this.rundate);
+        console.log('Next Rundate:' + this.nextRundate);
+
+        if (this.hasSystemError
+            | this.hasRDError
+            | this.hasNRDError) {
             this.isClearedToProceed = false;
         } else {
-            this.hasError = false;
             this.isClearedToProceed = true;
         }
-        console.log(this.selectedSystem);
+
         return this.isClearedToProceed;
     },
+    //below is a filler method which has no real use
     onSystemChange() {
       console.log(this.selectedSystem);
     }
